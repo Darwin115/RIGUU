a058ad079a5a958488b9df1110aa14e9faa488bb
Nicolás A. Flores <nikitoonsbug@gmail.com>
2024-11-08 12:06:41 -0600

    Tips and recipes

--- src/screens/TipsScreen.js
+++ src/screens/TipsScreen.js
@@ -1,24 +1,70 @@
-import { View, Text, Button } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import { Box } from "native-base";
 
 function TipsScreen({ navigation }) {
- 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "lightblue",
-      }}
-    >
-      <Text style={{ fontSize: 24 }}>Pantalla Prueva</Text>
+    <Box style={styles.container}>
+      {/* Fila 1 de botones */}
+      <View style={styles.buttonRow}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Tips")}
+          style={styles.button}
+        >
+          <Image source={require("../../assets/recipes.png")} style={styles.icon} />
+        </TouchableOpacity>
 
-      <Button
-        onPress={() => navigation.navigate("Noti")} // Asegúrate de que el nombre de la pantalla sea correcto
-        title="Boton 1"
-      />
-    </View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Tips")}
+          style={styles.button}
+        >
+          <Image source={require("../../assets/clothestips.png")} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.buttonRow}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Tips")}
+          style={styles.button}
+        >
+          <Image source={require("../../assets/medicinetips.png")} style={styles.icon} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Tips")}
+          style={styles.button}
+        >
+          <Image source={require("../../assets/electronicstips.png")} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    </Box>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#9CDBA6",
+  },
+  buttonRow: {
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#50B498",
+    width: 97,
+    height: 97,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+    marginHorizontal: 5,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+  },
+});
 
 export default TipsScreen;
